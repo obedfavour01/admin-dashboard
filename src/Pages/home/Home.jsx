7f1453cd9b6b50d9837c8236d44312bffb5a8f38
obed <obedfavour01@gmail.com>
2022-03-29 15:05:57 +0100

    Widget_Looking_Beautiful

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../../Components/Navbar/Navbar'
 import Sidebar from '../../Components/Sidebar/Sidebar'
+import Widgets from '../../Components/Widgets/Widgets'
 import './home.scss'
 
 function Home() {
@@ -9,6 +10,13 @@ function Home() {
       <Sidebar/>
       <div className="homeContainer">
         <Navbar/>
+        <div className="widgets">
+          <Widgets type = "user"/>
+          <Widgets type = "order"/>
+          <Widgets type = "earnings"/>
+          <Widgets type = "balance"/>
+
+        </div>
       </div>
     </div>
    
