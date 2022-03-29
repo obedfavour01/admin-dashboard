6572457d535383ccdd52e43680a6fefe6ef28276
obed <obedfavour01@gmail.com>
2022-03-29 16:24:00 +0100

    FeatureBoxWithProgressBar

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Featured from '../../Components/Featured/Featured'
+import Chart from '../../Components/Chart/Chart'
 import Navbar from '../../Components/Navbar/Navbar'
 import Sidebar from '../../Components/Sidebar/Sidebar'
 import Widgets from '../../Components/Widgets/Widgets'
@@ -8,6 +10,7 @@ function Home() {
   return (
     <div className='home'>
       <Sidebar/>
+
       <div className="homeContainer">
         <Navbar/>
         <div className="widgets">
@@ -15,9 +18,15 @@ function Home() {
           <Widgets type = "order"/>
           <Widgets type = "earnings"/>
           <Widgets type = "balance"/>
+        </div>
 
+        <div className="charts">
+          <Featured/>
+          <Chart/>
         </div>
       </div>
+
+
     </div>
    
   )
